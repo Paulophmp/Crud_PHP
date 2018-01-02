4d548a355d549c50cef32b0d5ee7ac9dca204460
Paulo Henrique <paulo.mendes00@hotmail.com>
2018-01-02 17:58:35 -0200

    "Clean code"

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,29 +1,17 @@
+<!-- resources/views/auth/login.blade.php -->
 
-    <!-- resources/views/auth/login.blade.php -->
+@extends('layout.master')
+@section('title', 'Login')
+@section('container')
 
-    @extends('layout.master')
-    @section('title', 'Login')
-    @section('container')
+    <div align="center" id="loading"></div>
+    <div id="divMsg"></div>
 
-        <div align="center" id="loading"></div>
-        <div id="divMsg"></div>
-
-        <div class="panel panel-primary">
-            <div class="panel-heading">Login</div>
-             <div class="panel-body">
-
-                 {{--@if(count($errors) > 0 )--}}
-{{--                     @if($errors->any())--}}
-                     {{--<ul class="alert alert-danger text-center">--}}
-                         {{--@foreach($errors->all() as $error)--}}
-                             {{--<ul>{{  $error }}</ul>--}}
-                         {{--@endforeach--}}
-                     {{--</ul>--}}
-                 {{--@endif--}}
-
-             <form method="POST" action="/auth/login" id="formLogin" class="form-horizontal">
+    <div class="panel panel-primary">
+        <div class="panel-heading">Login</div>
+        <div class="panel-body">
+            <form method="POST" action="/auth/login" id="formLogin" class="form-horizontal">
                 {!! csrf_field() !!}
-
                 <div class="form-group">
                     <label class="col-md-4 control-label" for="email">Email</label>
                     <div class="col-md-6">
@@ -34,7 +22,7 @@
                 <div class="form-group">
                     <label class="col-md-4 control-label" for="password">Senha</label>
                     <div class="col-md-6">
-                        <input type="password" class="form-control" name="password"  id="password">
+                        <input type="password" class="form-control" name="password" id="password">
                     </div>
                 </div>
 
@@ -54,46 +42,46 @@
                         <a href="{{ url('/password/email') }}">Esqueceu a Senha ?</a>
                     </div>
                 </div>
-             </form>
-             </div>
+            </form>
         </div>
-<script>
-    $(document).ready(function () {
-        $("#formLogin").validate({
-            rules: {
-                email: {
-                    required: true,
+    </div>
+    <script>
+        $(document).ready(function () {
+            $("#formLogin").validate({
+                rules: {
+                    email: {
+                        required: true,
+                    },
+                    password: {
+                        required: true,
+                    }
                 },
-                password: {
-                    required: true,
-                }
-            },
-            messages: {
-                email: {
-                    required: "Informe seu Email"
+                messages: {
+                    email: {
+                        required: "Informe seu Email"
+                    },
+                    password: {
+                        required: "Digite sua Senha"
+                    }
                 },
-                password: {
-                    required: "Digite sua Senha"
-                }
-            },
-            highlight: function(element) {
-                $(element).closest('.form-group').addClass('has-error');
-            },
-            unhighlight: function(element) {
-                $(element).closest('.form-group').removeClass('has-error');
-            },
-            errorElement: 'span',
-            errorClass: 'help-block',
-            success:function (label) {
-                label.text("ok").css("color", "green").removeClass("error").addClass("OK!!");
-            },
-            errorPlacement: function(error, element) {
-                if(element.parent('.input-group').length) {
-                    error.insertAfter(element.parent());
-                } else {
-                    error.insertAfter(element);
-                }
-            },
+                highlight: function (element) {
+                    $(element).closest('.form-group').addClass('has-error');
+                },
+                unhighlight: function (element) {
+                    $(element).closest('.form-group').removeClass('has-error');
+                },
+                errorElement: 'span',
+                errorClass: 'help-block',
+                success: function (label) {
+                    label.text("ok").css("color", "green").removeClass("error").addClass("OK!!");
+                },
+                errorPlacement: function (error, element) {
+                    if (element.parent('.input-group').length) {
+                        error.insertAfter(element.parent());
+                    } else {
+                        error.insertAfter(element);
+                    }
+                },
 //            submitHandler: function () {
 //                $.ajax({
 //                    type: "POST",
@@ -110,26 +98,25 @@
 //                        }
 //                })
 //            },
-            invalidHandler: function () {
-                toastr.error("Campos Obrigatórios");
-            }
-        });
-    });
-</script>
-<script type="text/javascript">
-    $(document).ready(function () {
-        $("#loginEntrar").click(function () {
-            $.ajax({
-                type: "POST",
-                data:{
-                },
-                success: function (data) {
-                    $("#divMsg").hide();
-                    $("#loading").html(data);
+                invalidHandler: function () {
+                    toastr.error("Campos Obrigatórios");
                 }
             });
-            $("#loading").html("<br><br> Carregando dados...<br><img src='/img/ajax.gif'> <br><br>").css({'color': 'Blue'},{'font-size':'35px'});
-        })
-    });
-</script>
-   @endsection
+        });
+    </script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $("#loginEntrar").click(function () {
+                $.ajax({
+                    type: "POST",
+                    data: {},
+                    success: function (data) {
+                        $("#divMsg").hide();
+                        $("#loading").html(data);
+                    }
+                });
+                $("#loading").html("<br><br> Carregando dados...<br><img src='/img/ajax.gif'> <br><br>").css({'color': 'Blue'}, {'font-size': '35px'});
+            })
+        });
+    </script>
+@endsection
--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -40,7 +39,7 @@
         <script type="text/javascript" src="/rhinoslider/js/easing.js"></script>
         <script src="/js/toastr.min.js"></script>
         <script>
-            $(document).ready(function(){
+            $(document).ready(function () {
                 $('[data-toggle="tooltip"]').tooltip();
             });
         </script>
@@ -54,7 +53,7 @@
         <nav class="block">
             <ul class="nav nav-pills pull-right">
                 {{--@if(Auth::user())--}}
-                    {{--<li role="presentation" class="active"><a href="/">Home</a></li>--}}
+                {{--<li role="presentation" class="active"><a href="/">Home</a></li>--}}
                 {{--@endif--}}
                 {{----}}
                 @if(Auth::user())
@@ -66,111 +65,126 @@
 
                 @endif
                 {{--<ul class="nav navbar-nav navbar-right">--}}
-                    <li>
-                        @if(Auth::check())
-{{--                            <p>{{Auth::user()->name}} <a href="{{url('auth/logout')}}">Sair</a></p>--}}
-                            {{--<a href="{{url('auth/logout')}}"  data-toggle="modal" data-target=".bs-example-modal-sm"  >--}}
-                                {{--<span class="glyphicon glyphicon-off red"></span>--}}
-                            {{--</a>--}}
-                            <a href="#"  data-toggle="modal" data-target=".bs-example-modal-sm">
-                                <span class="glyphicon glyphicon-off red" data-toggle="tooltip" title="Sair do Sistema" data-placement="bottom"></span>
-                            </a>
+                <li>
+                    @if(Auth::check())
+                        {{--                            <p>{{Auth::user()->name}} <a href="{{url('auth/logout')}}">Sair</a></p>--}}
+                        {{--<a href="{{url('auth/logout')}}"  data-toggle="modal" data-target=".bs-example-modal-sm"  >--}}
+                        {{--<span class="glyphicon glyphicon-off red"></span>--}}
+                        {{--</a>--}}
+                        <a href="#" data-toggle="modal" data-target=".bs-example-modal-sm">
+                            <span class="glyphicon glyphicon-off red" data-toggle="tooltip" title="Sair do Sistema"
+                                  data-placement="bottom"></span>
+                        </a>
 
                 {{--<li><a href="{{url('auth/logout')}}">Sair</a></li>--}}
-                        @else
-                            <li>
-                                {{--este que eu uso atualmente--}}
-                                <a href="{{url('auth/register')}}">Registro</a>
+                @else
+                    <li>
+                        {{--este que eu uso atualmente--}}
+                        <a href="{{url('auth/register')}}">Registro</a>
 
-                                <!-- Trigger the modal with a button -->
-                                <a type="button" class="" id="myBtn" data-toggle="modal" data-target="#myModalTeste">Registro</a>
-                                <!-- Modal -->
-                                <div class="modal fade" id="myModalTeste" role="dialog">
-                                    <div class="modal-dialog">
+                        <!-- Trigger the modal with a button -->
+                        <a type="button" class="" id="myBtn" data-toggle="modal"
+                           data-target="#myModalTeste">Registro</a>
 
-                                        <!-- Modal content-->
-                                        <div class="modal-content">
-                                            <div class="modal-header" style="padding:35px 50px;">
-                                                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                                <h4><span class="glyphicon glyphicon-lock"></span> Login</h4>
+                        <!-- Modal -->
+                        <div class="modal fade" id="myModalTeste" role="dialog">
+                            <div class="modal-dialog">
+                                <!-- Modal content-->
+                                <div class="modal-content">
+                                    <div class="modal-header" style="padding:15px 50px;">
+                                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                        <h4>
+                                            <span class="glyphicon glyphicon-lock"></span> Login
+                                        </h4>
+                                    </div>
+                                    <div class="modal-body" style="padding:40px 50px;">
+                                        <form method="POST" class="form-horizontal" action="{{url('/auth/register')}}">
+                                            {!! csrf_field() !!}
+                                            <div class="form-group has-feedback">
+                                                <label for="name">Nome</label>
+                                                <input type="text" class="form-control" id="name"
+                                                       value="{{ old('name') }}" placeholder="Enter Nome" name="name">
+                                                <span class="glyphicon glyphicon-user form-control-feedback"></span>
                                             </div>
-                                            <div class="modal-body" style="padding:40px 50px;">
-                                                <form method="POST" class="form-horizontal" action="{{url('/auth/register')}}">
-                                                    {!! csrf_field() !!}
-                                                    <div class="form-group has-feedback">
-                                                        <label for="name">Name</label>
-                                                        <input type="text" class="form-control" id="name" value="{{ old('name') }}" placeholder="Enter Nome" name="name">
-                                                        <span class="glyphicon glyphicon-user form-control-feedback"></span>
-                                                    </div>
 
-                                                    <div class="form-group">
-                                                        <label for="mail"><span class="glyphicon glyphicon-envelope"></span> E-mail</label>
-                                                        <input type="text" class="form-control" id="mail" value="{{ old('name') }}" placeholder="Enter email" name="email">
-                                                    </div>
-
-                                                    <div class="form-group">
-                                                        <label for="psw"><span class="glyphicon glyphicon-eye-open"></span> Password</label>
-                                                        <input type="password" class="form-control" id="psw" placeholder="Enter password" name="password">
-                                                    </div>
-
-                                                    <div class="form-group has-feedback">
-                                                        <label for="conf_psw"> Confirme Password</label>
-                                                        <input type="password" class="form-control" id="conf_psw" placeholder="" name="password_confirmation">
-                                                        <span class="glyphicon glyphicon glyphicon-log-in form-control-feedback"></span>
-                                                    </div>
-
-                                                    <div class="checkbox">
-                                                        <label><input type="checkbox" value="" checked>Remember me</label>
-                                                    </div>
-                                                    <button type="submit" class="btn btn-success btn-block"><span class="glyphicon glyphicon-off"></span> Registrar</button>
-                                                </form>
+                                            <div class="form-group">
+                                                <label for="mail"><span class="glyphicon glyphicon-envelope"></span>
+                                                    E-mail</label>
+                                                <input type="text" class="form-control" id="mail"
+                                                       value="{{ old('name') }}" placeholder="Enter email" name="email">
                                             </div>
-                                            <div class="modal-footer">
-                                                <button type="submit" class="btn btn-danger btn-default pull-left" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancelar</button>
-                                                <p>Not a member? <a href="#">Sign Up</a></p>
-                                                <p>Forgot <a href="#">Password?</a></p>
-                                            </div>
-                                        </div>
 
+                                            <div class="form-group">
+                                                <label for="psw"><span class="glyphicon glyphicon-eye-open"></span>
+                                                    Password</label>
+                                                <input type="password" class="form-control" id="psw"
+                                                       placeholder="Enter password" name="password">
+                                            </div>
+
+                                            <div class="form-group has-feedback">
+                                                <label for="conf_psw"> Confirme Password</label>
+                                                <input type="password" class="form-control" id="conf_psw" placeholder=""
+                                                       name="password_confirmation">
+                                                <span class="glyphicon glyphicon glyphicon-log-in form-control-feedback"></span>
+                                            </div>
+
+                                            <div class="checkbox">
+                                                <label><input type="checkbox" value="" checked>Remember me</label>
+                                            </div>
+                                            <button type="submit" class="btn btn-success btn-block"><span
+                                                        class="glyphicon glyphicon-off"></span> Registrar
+                                            </button>
+                                        </form>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="submit" class="btn btn-danger btn-default pull-left"
+                                                data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span>
+                                            Cancelar
+                                        </button>
+                                        <p>Not a member? <a href="#">Sign Up</a></p>
+                                        <p>Forgot <a href="#">Password?</a></p>
                                     </div>
                                 </div>
-                                {{--Fim Modal--}}
-                            </li>
-                        @endif
+
+                            </div>
+                        </div>
+                        {{--Fim Modal--}}
                     </li>
-                 {{--</ul>--}}
+                    @endif
+                    </li>
+                    {{--</ul>--}}
             </ul>
 
             <!-- Small modal -->
 
-            {{--<div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">--}}
-                {{--<div class="modal-dialog modal-sm" role="document">--}}
-                    {{--<div class="modal-content">--}}
+        {{--<div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">--}}
+        {{--<div class="modal-dialog modal-sm" role="document">--}}
+        {{--<div class="modal-content">--}}
 
-                        {{--<div class="modal-footer">--}}
-                            {{--<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>--}}
-                            {{--<button type="button" class="btn btn-primary">Save changes</button>--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-            {{--</div>--}}
+        {{--<div class="modal-footer">--}}
+        {{--<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>--}}
+        {{--<button type="button" class="btn btn-primary">Save changes</button>--}}
+        {{--</div>--}}
+        {{--</div>--}}
+        {{--</div>--}}
+        {{--</div>--}}
 
-            {{--<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">--}}
-                {{--Launch demo modal--}}
-            {{--</button>--}}
+        {{--<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">--}}
+        {{--Launch demo modal--}}
+        {{--</button>--}}
 
-            <!-- Modal -->
+        <!-- Modal -->
             <div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h6 class="modal-title" id="mySmallModalLabel">Acesso ao sistema !</h6>
+                            <h6 class="" id="mySmallModalLabel">Acesso ao sistema !</h6>
                         </div>
                         <div class="modal-body">
-                            <p>Deseja realmente sair do Sistema??</p>
+                            <p>Deseja realmente sair do Sistema?</p>
                         </div>
                         <div class="modal-footer">
-                            <a href="{{url('auth/logout')}}"  class="btn btn-primary">Sim</a>
+                            <a href="{{url('auth/logout')}}" class="btn btn-primary">Sim</a>
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Não</button>
                         </div>
                     </div>
@@ -181,12 +195,12 @@
         <h3 class="text-muted">Project name</h3>
     </div>
 
-        @yield('container')
+    @yield('container')
 
 </div> <!-- /container -->
 <hr>
 <footer>
-    <p>&copy; 2015 - <?= date('Y') ; ?> by Laravel Technologies Ltd. All rights reserved.</p>
+    <p>&copy; 2015 - <?= date('Y'); ?> by Laravel Technologies Ltd. All rights reserved.</p>
 </footer>
 </body>
 </html>
